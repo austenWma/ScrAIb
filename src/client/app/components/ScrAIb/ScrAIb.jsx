0cb03a47d3ee6c094313f2af0deca9e191655950
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-10-01 18:36:52 -0700

    Touched up style pn ScrAIb page

--- src/client/app/components/ScrAIb/ScrAIb.jsx
+++ src/client/app/components/ScrAIb/ScrAIb.jsx
@@ -2,9 +2,9 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 import {Redirect, Link} from 'react-router-dom'
 
+import ScrAIbMiddle from './ScrAIbMiddle.jsx'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import IconButton from 'material-ui/IconButton';
-import SettingsVoice from 'material-ui/svg-icons/action/settings-voice';
 
 class ScrAIb extends Component {
   constructor (props) {
@@ -20,17 +20,7 @@ class ScrAIb extends Component {
             Hello
         </div>
         <div className="scraibMiddleContainer">
-            <div className="scraibMiddleBorderContainer">
-            </div>
-            <div className="scraibRecordButtonContainer">
-                <div className="scraibRecordButtonCircleContainer">
-                    <IconButton iconStyle={{width: '100%', height: '70%', color: '#78e3fd'}} style={{width: '100%', height: '100%'}}>
-                        <SettingsVoice />
-                    </IconButton>
-                </div>
-            </div>
-            <div className="scraibMiddleBorderContainer">
-            </div>
+            <ScrAIbMiddle />
         </div>
         <div className="scraibRightContainer">
             Hello3
