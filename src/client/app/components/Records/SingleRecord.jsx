d6352afbabd82c18922da88bec6b5e960603b091
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-10-19 23:09:24 -0700

    formatted the records page

--- src/client/app/components/Records/SingleRecord.jsx
+++ src/client/app/components/Records/SingleRecord.jsx
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton'
+
 class SingleRecord extends Component {
     constructor (props) {
         super(props)
@@ -9,8 +12,23 @@ class SingleRecord extends Component {
     render() {
         let singleRecordObj = JSON.parse(this.props.singleRecord)
         return (
-            <div>
-                <div>{singleRecordObj.patient}</div>
+            <div className="singleRecordContainer">
+                <div className="singleRecordCircle">
+                </div>
+                <div className="singleRecordName">
+                    {singleRecordObj.patient}
+                </div>
+                <div className="singleRecordDate">
+                    {singleRecordObj.date}
+                </div>
+                <div className="singleRecordButtons">
+                    <div className="singleRecordViewButton">
+                        <RaisedButton label="View and Edit" primary={true} buttonStyle={{backgroundColor: '#78e3fd', width: '200'}}/>
+                    </div>
+                    <div>
+                        <RaisedButton label="Delete" primary={true} buttonStyle={{backgroundColor: '#DE5A5A'}}/>
+                    </div>
+                </div>
             </div>
         )
     }
