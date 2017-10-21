1ba4c26a604df78493289943b878ccee15ff7123
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-10-20 18:37:11 -0700

    Added modals for the records page

--- src/client/app/components/Records/SingleRecord.jsx
+++ src/client/app/components/Records/SingleRecord.jsx
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
+import RecordModal from './SingleRecordModal.jsx'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import RaisedButton from 'material-ui/RaisedButton'
 
 class SingleRecord extends Component {
     constructor (props) {
         super(props)
+        this.state = {
+            modalShow: false
+        }
+        this.modalOpen = this.modalOpen.bind(this)
+        this.modalClose = this.modalClose.bind(this)
+    }
+
+    modalOpen() {
+        this.setState({ modalShow: true })
+    }
+    modalClose() {
+        this.setState({ modalShow: false })
     }
 
     render() {
@@ -23,12 +37,14 @@ class SingleRecord extends Component {
                 </div>
                 <div className="singleRecordButtons">
                     <div className="singleRecordViewButton">
-                        <RaisedButton label="View and Edit" primary={true} buttonStyle={{backgroundColor: '#78e3fd', width: '200'}}/>
+                        <RaisedButton label="View and Edit" primary={true} buttonStyle={{backgroundColor: '#78e3fd', width: '200'}} onClick={this.modalOpen}/>
                     </div>
                     <div>
                         <RaisedButton label="Delete" primary={true} buttonStyle={{backgroundColor: '#DE5A5A'}}/>
                     </div>
                 </div>
+
+                <RecordModal show={this.state.modalShow} onHide={this.modalClose} />
             </div>
         )
     }
